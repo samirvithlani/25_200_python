64a8b948f42bdc270c64c373b369138788983035
samirvithlani <samir.vithlani83955@gmail.com>
2025-09-08 19:52:43 +0530

    arithmetic

---
+++ operators/arithmetic/ArithmeticDemo1.py
@@ -0,0 +1,22 @@
+# + , -,*,/, % ,//, **
+
+a = 10
+b = 3
+
+c = a + b
+print("add = ",c)
+
+c = a - b
+print("sub = ",c)
+
+c = a * b
+print("mul = ",c)
+
+c = a / b  # / -->return float only..
+print("div = ",c)
+
+c = a//b # floot div --> int
+print("floor div ",c)
+
+c = a **b #10 **3 = 10*10*10
+print("pow = ",c)
