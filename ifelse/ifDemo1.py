fc3c2e70d52d87a4fa3edf33ba41d6b44fc2743e
samirvithlani <samir.vithlani83955@gmail.com>
2025-09-10 19:24:53 +0530

    if else

---
+++ ifelse/ifDemo1.py
@@ -0,0 +1,14 @@
+#is this no is positive or neg
+no = -100
+
+if no>0:
+    print("this no is positive")
+    print("this is if part..")        
+    print("this is if block...")
+else:
+    print("this no is neg..")
+    print("this is else block...")
+    
+    
+    
+    
---
+++ ifelse/ifDemo2.py
@@ -0,0 +1,7 @@
+#get no from user and check no is even or odd::
+no = int(input("enter no :")) #int
+#15 /2 == rem -->0
+if no % 2 ==0:
+    print("this is even no")
+else:
+    print("this is odd no")    
