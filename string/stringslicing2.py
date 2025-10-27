bd04bb2a08ddb1425460cb86c457b69a92b1ebcd
samirvithlani <samir.vithlani83955@gmail.com>
2025-10-27 21:04:08 +0530

    slcing

---
+++ string/stringslicing2.py
@@ -0,0 +1,9 @@
+#4 step value
+#string[0:4:1] 0 include 4 exlude 1
+text = "Python"
+
+print(text[::2]) #starts from 0 goto end and 2 incr
+print(text[::-1])
+
+#combination
+print(text[1:5:2]) #1st index start go till 4th and increment by 2
---
+++ string/stringslicingtask1.py
@@ -0,0 +1,6 @@
+s ="ArtificialIntelligence"
+print(s[0:4]) #Arti
+print(s[-3:]) #nce
+print(s[1:])  #
+print(s[:-1]) #0 --> -1
+print(s[2:5:2])
