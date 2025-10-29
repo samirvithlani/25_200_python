557bca472f80df5a762c028e3e970573b049e6a1
samirvithlani <samir.vithlani83955@gmail.com>
2025-10-29 19:38:19 +0530

    list

---
+++ list/listDemo1.py
@@ -0,0 +1,33 @@
+#empty list
+data = []
+print(data)
+print(type(data))
+
+users = ["ram","shyam","amit","sumit","raj","amit"]
+print(users)
+
+#index..
+print("users[0]",users[0])
+
+
+#find size of list
+print("length..",len(users))
+#range for loop
+
+# for i in range(0,len(users)):
+#     print(users[i])
+    
+print("----------------------------")    
+
+#for each..
+
+# for i in users:
+#     print(i)
+
+
+#mutable
+print("0",users[0])    
+users[0]="RAM"
+print(users)
+
+    
---
+++ string/slicingtask3.py
@@ -0,0 +1,38 @@
+text = "PythonProgramming"
+print(text[0:6])   
+
+
+text = "PythonProgramming"
+print(text[6:])    
+
+
+text = "PythonProgramming"
+print(text[:6])    
+
+
+text = "PythonProgramming"
+print(text[::2])   
+
+
+text = "PythonProgramming"
+print(text[::-1])  
+
+
+text = "PythonProgramming"
+print(text[-11:-6])  
+
+
+text = "PythonProgramming"
+print(text[2:10:2])  
+
+
+text = "PythonProgramming"
+print(text[-1:-10:-2])  
+
+
+text = "PythonProgramming"
+print(text[1:-1])  
+
+
+text = "PythonProgramming"
+print(text[0:len(text)//2])  
