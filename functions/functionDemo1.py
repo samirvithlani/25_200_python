c86963ac869c1071a4ae219e73f3f9bc84206f83
samirvithlani <samir.vithlani83955@gmail.com>
2025-12-24 19:34:05 +0530

    function

---
+++ functions/functionDemo1.py
@@ -0,0 +1,31 @@
+#without return type without argument...
+def demo():
+    print("demo function called....")
+
+#call...
+demo()    
+
+def drawLine():
+    print("__________________________________________")
+
+
+# print("Hello")
+# print("---------------------------------------")
+# print("Hi")
+# print("---------------------------------------")
+# print("Good Evening")
+# print("---------------------------------------")
+# print("INdia")
+# print("---------------------------------------")
+# print("abcd")
+    
+
+print("Hello")
+drawLine()
+print("Hi")
+drawLine()
+print("Good Evening")
+drawLine()
+print("INdia")
+drawLine()
+print("abcd")    
---
+++ functions/functionDemo2.py
@@ -0,0 +1,15 @@
+#argument...
+
+#a = 10 
+#b = 20
+def add(a,b):
+    print("add function called...")
+    print(a+b)
+
+
+#add() #error     TypeError: add() missing 2 required positional arguments: 'a' and 'b
+add(100,20)
+x =1000
+y = 2000
+
+add(x,y)
