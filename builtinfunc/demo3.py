c6003c94b241c6a29c274440b23cd8bd338797d7
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-18 19:28:24 +0530

    demo3

---
+++ builtinfunc/demo3.py
@@ -0,0 +1,14 @@
+#min max
+
+data = [100,20,30,40,123,456,908]
+print(min(data))
+print(max(data))
+
+names = ["ajay","raj","sumit","zarna","parth"]
+print(min(names))
+print(max(names))
+print(min(names,key=len))
+print(max(names,key=len))
+
+#sum
+print(sum(data))
