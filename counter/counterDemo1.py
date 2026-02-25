4d8e36d706057d5340530d5f98b6783bb9b4a8e1
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-25 19:34:37 +0530

    counter

---
+++ counter/counterDemo1.py
@@ -0,0 +1,4 @@
+from collections import Counter
+sports = ["circket","football","circket","chess","running"]
+print(sports.count("circket"))
+print(Counter(sports))
---
+++ counter/counterDemo2.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+sports = [
+"cricket","football","cricket","badminton",
+"football","cricket","tennis","cricket",
+"badminton","football","football"
+]
+#count frequecny
+sports_count = Counter(sports)
+print(sports_count)
+
+#most popular
+most_popular = sports_count.most_common(1)
+print(most_popular)
+
+# #sports only selected by 1 player
+# oneplayer = sum(1 for s in sports_count if sports_count[s]==1)
+# print(oneplayer)
+
+match_type = [
+"5v5","7v7","5v5","11v11",
+"5v5","7v7","5v5","11v11",
+"7v7","5v5"
+]
+
+#most demaanded match
+#11v11 demanded ? >2 
